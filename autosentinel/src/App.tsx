148bbecd4b6538da80369f2d0503431e9f318799
Miguel Augusto <miguelmasche@gmail.com>
2024-06-06 16:26:35 -0700

    se4t up vite and tailwind.ss

---
+++ autosentinel/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Welcome to AutoSentinel
+      </h1>
+    </div>
+  );
+}
+
+export default App;
